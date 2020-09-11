074e9704504cb1e2a40936b726d4991052f02fa5
mnelson86 <63618951+mnelson86@users.noreply.github.com>
2020-09-11 11:21:16 -0400

    Update Store class with filled-out callback functions

--- db/store.js
+++ db/store.js
@@ -11,12 +11,33 @@ const writeFileAsync = util.promisify(fs.writeFile);
 //5 callback functions: read, write, getNotes, addNote, and removeNote
 //all return a value
 class Store {
-  read();
-  write();
-  getNotes();
-  addNote();
+  read(note) {
+    readFileAsync
+  };
+  write(note) {
+    writeFileAsync
+  };
+  getNotes(note){
+    this.read().then({
+      const gotNotes= JSON.parse(note)
+      }
+      return gotNotes;
+            
+    )};
+  addNote(note) {
+    const { title, text } = note;
+    if (!title || !text) {
+      throw new Error("Note 'title' and 'text' cannot be blank");
+    }
+    // Add a unique id to the note using uuid package
+    const newNote = { title, text, id: uuidv1() };
+    // Get all notes, add the new note, write all the updated notes, return the newNote
+    return this.getNotes()
+      .then(notes => [...notes, newNote])
+      .then(updatedNotes => this.write(updatedNotes))
+      .then(() => newNote);
+  }
   removeNote();
 }
 
-//export = new store
 module.exports = new store();
